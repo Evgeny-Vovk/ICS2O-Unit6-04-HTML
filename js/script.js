3291dc1d5aba729eefd2fe0c169d2d145651b363
Evgeny Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-06-25 05:58:06 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2022 Marshall All rights reserved
+//
+// Created by: Marshall
+// Created on: June 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit6-04-HTML/sw.js", {
+    scope: "/ICS2O-Unit6-04-HTML/",
+  })
+}
+
+"use strict"
+
+window.onload = function() {
+  // this calculates volume of a pyramid
+
+  const params = new URLSearchParams(document.location.search)
+  
+  // input
+  const radiusSphere = params.get('r')
+
+  // process
+  const volume = 4/3 * Math.PI * Math.pow(radiusSphere, 3)
+  const dimensions = "<ul>\n<li>r = " + radiusSphere
+
+  // output
+  document.getElementById('dimensions').innerHTML = dimensions
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+}
